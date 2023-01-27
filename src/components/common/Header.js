8bca0bf1173a1e5ba991d8290017331bc463d744
choj96 <choj9603@gmail.com>
2023-01-27 12:12:36 +0900

    reset.scss 추가 / font 추가 / header.scss 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,37 +1,40 @@
 import { Link } from "react-router-dom";
 /*
-a 태그 href 를 이용하면 페이지갱신(MPA)
-Link 컴포넌트를 이용하면 컴포넌트 갱신(SPA)
-: a 태그로 자동 변환이 된다.
-: to="uri" 라는 props 가 필요하다.
+  a 태그 href 를 이용하면 페이지갱신(MPA)
+  Link 컴포넌트를 이용하면 컴포넌트 갱신(SPA)
+  : a 태그로 자동 변환이 된다.
+  : to="uri" 라는 props 가 필요하다.
 */
 
 const Header = () => {
   return (
     <header>
-      <h1>
-        <Link to="/">Logo</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/department">Department</Link>
-        </li>
-        <li>
-          <Link to="/community">Community</Link>
-        </li>
-        <li>
-          <Link to="/gallery">Gallery</Link>
-        </li>
-        <li>
-          <Link to="/youtube">Youtube</Link>
-        </li>
-        <li>
-          <Link to="/location">Location</Link>
-        </li>
-        <li>
-          <Link to="/join">Join</Link>
-        </li>
-      </ul>
+      {/* 레이아웃 div */}
+      <div className="inner">
+        <h1>
+          <Link to="/">Logo</Link>
+        </h1>
+        <ul id="gnb">
+          <li>
+            <Link to="/department">Department</Link>
+          </li>
+          <li>
+            <Link to="/community">Community</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/youtube">Youtube</Link>
+          </li>
+          <li>
+            <Link to="/location">Location</Link>
+          </li>
+          <li>
+            <Link to="/join">Join</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
