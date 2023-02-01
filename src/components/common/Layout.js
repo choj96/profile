d369486bdca4254f956dfb902f2b873500e12632
choj96 <choj9603@gmail.com>
2023-02-01 10:20:16 +0900

    useRef 사용

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,20 +1,30 @@
-import React from "react";
-
-// Layout.js :공통요소를 적용하기 위한 처리 컴포넌트
+import React, { useEffect, useRef } from "react";
 
 const Layout = (props) => {
+  // useRef 는 변수입니다. 즉, state가 아닙니다.
+  // 아래 구문은 real Dom 을 참조함.
+  // 하지만 아직 real Dom 이 만들어 지지 않았으므로
+  // 참조가 어렵다.
+  // useRef(document.querySelector("section"));
+  // 순서 1번
+  const frame = useRef(null);
+  useEffect(() => {
+    // frame useRef 를 활용해서 section 태그를 참조해서 css 작업
+    frame.current.classList.remove("on");
+    frame.current.classList.add("on");
+
+    return () => {
+      // 아래 구문은 에러가 발생합니다.
+      // unmount 가 되면 참조 요소가 null 이된다.
+      // frame.current.classList.remove("");
+    };
+  }, []);
   return (
-    // 백틱을 이용해서 props 로 전달된 값을
-    // 이용해서 className 으로 활용
-    <section className={`content ${props.title}`}>
+    // 순서 2. 상위에 만들어둔 useRef 변수 frame 을 ref 속성으로 참조
+    <section className={`content ${props.title}`} ref={frame}>
       <figure></figure>
       <div className="inner">
         <h1>{props.title}</h1>
-        {/* 
-        실제 컴포넌트 내용
-        props.children 을 이용해서
-        원하는 영역에 컴포넌트를 배치가능 
-        */}
         {props.children}
       </div>
     </section>
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../common/Layout";
 
 const Community = () => {
+  // javascript 코드 자리
+  // 컴포넌트가 마운트, 업데이트(state 변화), 언마운트 시
+  // 하고 싶은 일을 작성하는 Hook useEffect
+  // 기본형태
+  // useEffect(() => {
+  //   하고싶은 일
+  //   클린업 함수 : 컴포넌트 제거시 실행
+  //   return () => {}
+  // }, [의존성배열: dependency Array]);
+  useEffect(() => {
+    console.log("mount : 컴포넌트 생성");
+    // axios, html 요서 선택하는 작업
+    return () => {
+      console.log("unmount : 컴포넌트 제거");
+    };
+  }, []);
+
+  // jsx 코드 자리
+  // : 주의 사항 return ()  o
+  // : 주의 사항 return {}  x
   return <Layout title={"Community"}>Community</Layout>;
 };
 
