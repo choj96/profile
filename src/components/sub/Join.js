f03d69a5581583705ff2cf254b5bd9958fabafd9
choj96 <choj9603@gmail.com>
2023-02-09 11:36:12 +0900

    Daum Post 연동 및 전화번호, 생년월일 - 자동입력

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react";
+import DaumPostcodeEmbed from "react-daum-postcode";
 import Layout from "../common/Layout";
-
 const Join = () => {
   // 회원 가입을 위한 정보를 한개의 객체로 관리
   // 정보 관리 초기객체
   let initVal = {
     userid: "",
     email: "",
+    phone: "",
     password: "",
     password2: "",
+    address: "",
+    address2: "",
     gender: "",
+    brithday: "",
     interests: "",
     edu: "",
+    hobby: null,
+    comment: "",
   };
   const [val, setVal] = useState(initVal);
 
-  // form에 입력된 name 과 value 를 이용해서 state 를 업데이트 하겠다.
-  // 매번 onChnange 가 별도 작성이 아닌 한 곳에서 관리하겠다.
+  // form 에 입력된 name 과 value 를 이용해서 state 를 업데이트 하겠다.
+  // 매번 onChange 가 별도 작성이 아닌 한 곳에서 관리하겠다.
   // state 업데이트를 한곳에서 관리하겠다.
   const handleChange = (e) => {
-    // console.log(e.target); // tag = {name: "userid",value="123"}
-    // console.log(e.target.name); // tag name = "userid"
-    // console.log(e.target.value); // tag value
+    // console.log(e.target); // tag = {name:"userid", value:"123"}
+    // console.log(e.target.name); // tag name="userid"
+    // console.log(e.target.value);// tag value
     const { name, value } = e.target;
     setVal({ ...val, [name]: value });
   };
+
   // 성별 라디오 이벤트 핸들러
   const handleRadio = (e) => {
-    // id 를 받아서 처리?
+    // id 를 받아서 처리할까? 고민중
     const { name, id } = e.target;
     // const isCheck = e.target.checked;
     setVal({ ...val, [name]: id });
   };
+
   // 관심사 체크박스 이벤트 핸들러
   const handleCheck = (e) => {
     // 사용자가 항목을 변경했는지 파악
@@ -39,28 +47,88 @@ const Join = () => {
     const { name } = e.target;
     // 다중으로 선택하여서 처리하는 법
     const inputs = e.target.parentElement.querySelectorAll("input");
-    inputs.forEach((item) => {
+    let data = {};
+    for (let item of inputs) {
       // 각각의 항목이 체크가 되었는지를 확인하는 용도
-      // let { name, checked } = item;
-      // console.log(name);
-      // console.log(checked);
-      // setVal({ ...val, [name]: checked });
+      let { id, checked } = item;
+      data[id] = checked;
+
+      // 1개라도 체크가 되었는지 검증
       if (item.checked) isCheck = true;
+    }
+    // setVal({ ...val, [name]: isCheck });
+    // prev state 활용
+    setVal((prev) => {
+      const obj = { ...prev };
+      obj.hobby = data;
+      return obj;
     });
-    setVal({ ...val, [name]: isCheck });
+
+    setVal((prev) => {
+      const obj = { ...prev };
+      obj[name] = isCheck;
+      return obj;
+    });
+  };
+
+  // Daum Post 연동 이벤트 핸들러
+  // 우편번호 출력
+  // 처음에는 안보이도록 state 설정
+  const [postVisible, setPostVisible] = useState(false);
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    // console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    // 주소 state 업데이트
+    setVal({ ...val, address: fullAddress });
+    setPostVisible(false);
+  };
+
+  // 전화번호 입력시 - 값 자동적용
+  const handlePhone = (e) => {
+    const { name } = e.target;
+    let value = e.target.value;
+    value = value
+      .replace(/[^0-9]/g, "") // 숫자를 제외한 모든 문자 제거
+      .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+    setVal({ ...val, [name]: value });
+  };
+
+  // 생년월일
+  const handleBirthday = (e) => {
+    const { name } = e.target;
+    let value = e.target.value;
+    value = value.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+    setVal({ ...val, [name]: value });
   };
 
   // 에러 정보 관리 객체
-  const [err, setErr] = useState({});
+  const [Err, setErr] = useState({});
   const check = (_val) => {
     const errs = {};
-    // 아이디체크
+    // 아이디 체크
     if (_val.userid.length < 5) {
-      errs.userid = "아이디를 5글자 이상 입력해주세요";
+      errs.userid = "아이디를 5글자 이상 입력해주세요.";
     }
     // 이메일 체크/이메일 정규표현식 이용한 처리
     if (_val.email.length < 8 || !/@/.test(_val.email)) {
       errs.email = "이메일은 최소 8글자 이상 @을 포함해 주세요.";
+    }
+    // 전화번호 체크
+    if (_val.phone === "") {
+      errs.phone = "전화번호를 입력해주세요.";
     }
     // 비밀번호
     const eng = /[a-zA-Z]/;
@@ -73,11 +141,15 @@ const Join = () => {
       !spc.test(_val.password)
     ) {
       errs.password =
-        "비밀번호는 5글자 이상, 영문 , 숫자 , 특수문자를 모두 포함해 주세요.";
+        "비밀번호는 5글자 이상, 영문, 숫자, 특수문자를 모두 포함해 주세요.";
     }
     // 비밀번호2 체크
     if (_val.password !== _val.password2 || !_val.password2) {
       errs.password2 = "비밀번호를 동일하게 입력해주세요.";
+    }
+    // 생년월일
+    if (_val.birthday === "") {
+      errs.birthday = "생년월일을 입력하세요.";
     }
     // 성별 체크
     if (_val.gender === "") {
@@ -91,6 +163,17 @@ const Join = () => {
     if (_val.edu === "") {
       errs.edu = "학력을 선택해주세요.";
     }
+    // 기타내용 체크
+    if (_val.comment.length < 20) {
+      errs.comment = "남기는 말을 20글자 이상 입력해주세요.";
+    }
+    // 주소 체크
+    if (_val.address === "") {
+      errs.address = "주소를 입력해주세요";
+    }
+    if (_val.address2 === "") {
+      errs.address = "상세주소를 입력해주세요";
+    }
     return errs;
   };
 
@@ -99,8 +182,8 @@ const Join = () => {
     console.log(val);
   }, [val]);
   useEffect(() => {
-    console.log(err);
-  }, [err]);
+    console.log(Err);
+  }, [Err]);
 
   // 전송 실행시 각 항목의 내용 체크
   const handleSubmit = (e) => {
@@ -108,18 +191,19 @@ const Join = () => {
     // SPA 컨셉과 맞지 않는다.
     // state 도 초기화가 된다.
     e.preventDefault();
-    // 필요 항목에 대한 체크 실행
+    // 필요항목에 대한 체크 실행
     // 각 항목 체크용 객체를 생성해 진행
     setErr(check(val));
   };
-  // 에러 및 유효성 검사 결과 한개의  객체로 관리
+
+  // 에러 및 유효성 검사 결과 한개의 객체로 관리
   return (
     <Layout title={"Join"}>
       <form onSubmit={handleSubmit}>
         <fieldset>
-          <legend>회원가입</legend>
+          <legend>회원 가입</legend>
           <table>
-            <caption>회원가입 정보 입력</caption>
+            <caption>회원 가입 정보 입력</caption>
             <tbody>
               {/* 아이디 입력 */}
               <tr>
@@ -127,7 +211,7 @@ const Join = () => {
                   <label htmlFor="userid">USER ID</label>
                 </th>
                 <td>
-                  {/* http://localghost/join?userid=홍길동 */}
+                  {/* http://localhost/join?userid=홍길동 */}
                   <input
                     type="text"
                     id="userid"
@@ -135,7 +219,7 @@ const Join = () => {
                     placeholder="아이디를 입력하세요."
                     onChange={handleChange}
                   />
-                  <span className="err">{err.userid}</span>
+                  <span className="err">{Err.userid}</span>
                 </td>
               </tr>
               {/* 이메일 */}
@@ -151,13 +235,31 @@ const Join = () => {
                     placeholder="이메일 주소를 입력해주세요."
                     onChange={handleChange}
                   />
-                  <span className="err">{err.email}</span>
+                  <span className="err">{Err.email}</span>
+                </td>
+              </tr>
+              {/* 전화번호 */}
+              <tr>
+                <th>
+                  <label htmlFor="phone">PHONE</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    placeholder="전화번호를 입력하세요."
+                    maxLength={13}
+                    onChange={handlePhone}
+                    value={val.phone}
+                  />
+                  <span className="err">{Err.phone}</span>
                 </td>
               </tr>
               {/* 비밀번호 */}
               <tr>
                 <th>
-                  <label htmlFor="password">PASSWORD</label>
+                  <label>PASSWORD</label>
                 </th>
                 <td>
                   <input
@@ -167,7 +269,7 @@ const Join = () => {
                     placeholder="비밀번호를 입력하세요"
                     onChange={handleChange}
                   />
-                  <span className="err">{err.password}</span>
+                  <span className="err">{Err.password}</span>
                 </td>
               </tr>
               {/* 비밀번호2 */}
@@ -183,7 +285,67 @@ const Join = () => {
                     placeholder="비밀번호를 입력하세요"
                     onChange={handleChange}
                   />
-                  <span className="err">{err.password2}</span>
+                  <span className="err">{Err.password2}</span>
+                </td>
+              </tr>
+              {/* 주소입력 : https://www.npmjs.com/package/react-daum-postcode */}
+              <tr>
+                <th>
+                  <label htmlFor="address">Address</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    placeholder="주소를 입력해주세요."
+                    onChange={handleChange}
+                    readOnly
+                    onClick={() => setPostVisible(true)}
+                    style={{ cursor: "pointer" }}
+                    value={val.address}
+                  />
+                  <span className="err">{Err.address}</span>
+                  {/* Daum Post 컴포넌트 출력 자리 */}
+                  {postVisible ? (
+                    <div style={{ position: "absolute", background: "#fff" }}>
+                      <button onClick={() => setPostVisible(false)}>
+                        닫기
+                      </button>
+                      <DaumPostcodeEmbed
+                        onComplete={handleComplete}
+                        style={{ width: 400, height: 600 }}
+                      />
+                    </div>
+                  ) : null}
+
+                  <br />
+                  <input
+                    type="text"
+                    id="address2"
+                    name="address2"
+                    placeholder="상세주소를 입력해주세요."
+                    onChange={handleChange}
+                  />
+                  <span className="err">{Err.address2}</span>
+                </td>
+              </tr>
+              {/* 생년월일 */}
+              <tr>
+                <th>
+                  <label htmlFor="birthday">BIRTHDAY</label>
+                </th>
+                <td>
+                  <input
+                    type="date"
+                    id="birthday"
+                    name="birthday"
+                    placeholder="생년월일을 입력해주세요."
+                    onChange={handleBirthday}
+                    value={val.brithday}
+                    maxLength={13}
+                  />
+                  <span className="err">{Err.birthday}</span>
                 </td>
               </tr>
               {/* 성별체크 */}
@@ -204,12 +366,12 @@ const Join = () => {
                     name="gender"
                     onChange={handleRadio}
                   />
-                  <span className="err">{err.gender}</span>
+                  <span className="err">{Err.gender}</span>
                 </td>
               </tr>
               {/* 관심사 */}
               <tr>
-                <th>INTEREST</th>
+                <th>INTERESTS</th>
                 <td>
                   <label htmlFor="sports">Sports</label>
                   <input
@@ -239,28 +401,45 @@ const Join = () => {
                     name="interests"
                     onChange={handleCheck}
                   />
-                  <span className="err">{err.interests}</span>
+                  <span className="err">{Err.interests}</span>
                 </td>
               </tr>
               {/* 교육경력 */}
               <tr>
-                <th>EDUCATiON</th>
+                <th>EDUCATION</th>
                 <td>
-                  <select naem="edu" id="edu" onChange={handleChange}>
+                  <select name="edu" id="edu" onChange={handleChange}>
                     <option value="">학력을 선택하세요.</option>
                     <option value="step-1">초등학교 졸업</option>
                     <option value="step-2">중학교 졸업</option>
                     <option value="step-3">고등학교 졸업</option>
                     <option value="step-4">대학교 졸업</option>
                   </select>
-                  <span className="err">{err.edu}</span>
+                  <span className="err">{Err.edu}</span>
+                </td>
+              </tr>
+              {/* 기타의견 */}
+              <tr>
+                <th>
+                  <label htmlFor="comment">COMMENT</label>
+                </th>
+                <td>
+                  {/* 단순한 글자입력만 됩니다. WYSIWYG Editor 가 필요 */}
+                  <textarea
+                    name="comment"
+                    id="commnet"
+                    cols="30"
+                    rows="5"
+                    placeholder="남기는 말을 입력해주세요."
+                    onChange={handleChange}
+                  ></textarea>
+                  <span className="err">{Err.comment}</span>
                 </td>
               </tr>
               {/* 폼 전송 */}
               <tr>
                 <th colSpan="2">
-                  {/* <button type="button">전송</button> 
-                  주의사항 : 폼 안에서는 button type= "submit"이 기본*/}
+                  {/* <button type="button">전송</button> */}
                   <input type="reset" value="RESET" />
                   <input type="submit" value="SUBMIT" />
                 </th>
