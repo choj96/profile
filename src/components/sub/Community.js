b5f33e7b06227b4383302724f7ab7297a4d4d4da
choj96 <choj9603@gmail.com>
2023-02-13 13:19:38 +0900

    Community 완료

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,21 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../common/Layout";
 import CommunityCard from "./CommunityCard";
+
 // https://react-hook-form.com/
 // npm install react-hook-form
 // https://github.com/jquense/yup/tree/pre-v1
 // npm install -S yup
 // npm install @hookform/resolvers
 
-// 01. useForm Import
+// 01. useForm import
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-// 02.form 요소의 항목별 에러 체크
+// 02. form 요소의 항목별 에러 체크 정의
 const schema = yup.object({
-  title: yup.string().trim().required("제목을 입력해 주세요."),
-  content: yup.string().trim().required("내용을 입력해 주세요."),
+  title: yup.string().trim().required("제목을 입력해주세요."),
+  content: yup.string().trim().required("내용을 입력해주세요."),
+  timestamp: yup.string().required("날짜를 선택해 주세요"),
 });
 
 const Community = () => {
@@ -23,17 +25,17 @@ const Community = () => {
   // register : 각각의 form 의 name 을 설정
   // handleSubmit : form 에서 onSubmit 할때 실행됨
   // reset : form 에서 reset 할때 실행
-  // formState:{errors} : yup 에러 출력 활용
-
+  // formState: { errors }  yup 에러 출력 활용
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema), // yup과 연결 시켜줌
-    mode: "onChange", // mode 가 onChange 면 실행하라
+    resolver: yupResolver(schema), // yup 과 연결 시켜줌.
+    mode: "onChange", // mode 가 onChange 면 실행하라..
   });
+
   // 데모용 데이터 생성
   const initPost = [
     { title: "Hello 1", content: "Welocome To React!" },
@@ -42,16 +44,24 @@ const Community = () => {
     { title: "Hello 4", content: "Welocome To React!" },
     { title: "Hello 5", content: "Welocome To React!" },
   ];
-  const [posts, setPosts] = useState(initPost);
 
-  const inputEdit = useRef(null);
-  const textareaEdit = useRef(null);
+  // 로컬에 저장된 내용을 가지고 온다.
+  const getLocalPost = () => {
+    const data = localStorage.getItem("post");
+    if (data === null) {
+      return [];
+    } else {
+      return JSON.parse(data);
+    }
+  };
+  const [posts, setPosts] = useState(getLocalPost());
+
   const [Allowed, setAllowed] = useState(true);
-
   const createPost = (data) => {
+    // data ======>  { title: title, content: conten}
     setPosts([...posts, data]);
     // ...register("title")
-    // ...register("content")
+    // ...register("conente")
     reset();
 
     setAllowed((prev) => true);
@@ -68,7 +78,6 @@ const Community = () => {
   // 삭제기능
   const deletePost = (idx) => {
     if (!window.confirm("정말 삭제하시겠습니까?")) {
-      console.log("해");
       return;
     }
     setPosts(posts.filter((item, index) => idx !== index));
@@ -86,7 +95,6 @@ const Community = () => {
       })
     );
   };
-
   // 업데이트 취소
   const disapleUpdate = (idx) => {
     setAllowed(true);
@@ -99,19 +107,15 @@ const Community = () => {
       })
     );
   };
-
   // 게시물 업데이트
-  const updatePost = (idx) => {
-    if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
-      inputEdit.current.value = "";
-      textareaEdit.current.value = "";
-      return alert("수정할 제목과 내용을 입력해주세요.");
-    }
+  const updatePost = (data) => {
     setPosts(
       posts.map((item, index) => {
-        if (idx === index) {
-          item.title = inputEdit.current.value;
-          item.content = textareaEdit.current.value;
+        // 숫자로 변경하여서 비교
+        if (parseInt(data.index) === index) {
+          item.title = data.title;
+          item.content = data.content;
+          item.timestamp = data.timestamp;
           item.enableUpdate = false;
         }
         return item;
@@ -121,14 +125,15 @@ const Community = () => {
     setAllowed(true);
   };
 
-  // 디버깅
+  // 로컬에 저장
   useEffect(() => {
-    console.log(posts);
+    localStorage.setItem("post", JSON.stringify(posts));
   }, [posts]);
 
   return (
     <Layout title={"Community"}>
       {/* 입력폼 */}
+
       <div className="inputBox">
         <form onSubmit={handleSubmit(createPost)}>
           <input
@@ -145,6 +150,10 @@ const Community = () => {
             {...register("content")}
           ></textarea>
           <span className="err">{errors.content?.message}</span>
+          <br />
+          <input type="date" {...register("timestamp")} />
+          <span className="err">{errors.timestamp?.message}</span>
+          <br />
           <div className="btnSet">
             {/* form 안쪽에 버튼은 type 을 정의한다. */}
             <button type="reset">CANCEL</button>
@@ -159,8 +168,6 @@ const Community = () => {
             <CommunityCard
               key={index}
               item={item}
-              inputEdit={inputEdit}
-              textareaEdit={textareaEdit}
               disapleUpdate={disapleUpdate}
               updatePost={updatePost}
               enableUpdate={enableUpdate}
